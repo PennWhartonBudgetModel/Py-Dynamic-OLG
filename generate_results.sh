b571337f04684c8ac3b09a264a178afcc7868a31
Pete <peterlu@wharton.upenn.edu>
2016-10-19 13:19:27 -0400

    Updated job submission script to skip non-zero gcut baseline runs.

--- generate_results.sh
+++ generate_results.sh
@@ -15,12 +15,22 @@ mkdir -p ${LOGDIR}
 
 
 # Submit closed economy runs
-for GCUT in 0.10 0.05 0.00 -0.05; do
+# (Steady state and open economy runs executed as dependencies)
+for GCUT in +0.00 +0.10 +0.05 -0.05; do
   for PLAN in base trump clinton ryan; do
-    qsub -N solve_closed -t 1-16 -q short.q -j y -o ${LOGDIR}'/closed_inddeep=$TASK_ID_plan='${PLAN}'_gcut='${GCUT}'.log' -b y 'matlab -nojvm -nosplash -r "solve_closed(inddeep_to_params(${SGE_TASK_ID}), '\'${PLAN}\'', '${GCUT}', false)"'
+    
+    # Skip non-zero gcut baseline runs
+    if [[ (${PLAN} == base) && (${GCUT} != +0.00) ]]; then continue; fi
+    
+    qsub -N solve_closed -t 1-16 -q short.q \
+         -j y -o ${LOGDIR}'/closed_inddeep=$TASK_ID_plan='${PLAN}'_gcut='${GCUT}'.log' \
+         -b y 'matlab -nojvm -nosplash -r "solve_closed(inddeep_to_params(${SGE_TASK_ID}), '\'${PLAN}\'', '${GCUT}', false)"'
+		
   done
 done
 
 
 # Process and package results
-qsub -N package -hold_jid solve_closed -q short.q -j y -o ${LOGDIR}'/package.log' -b y 'matlab -nojvm -nosplash -r "check_closed_convergence, generate_static_aggregates_closed, package_results"'
+qsub -N package -hold_jid solve_closed -q short.q \
+     -j y -o ${LOGDIR}'/package.log' \
+     -b y 'matlab -nojvm -nosplash -r "check_closed_convergence, generate_static_aggregates_closed, package_results"'
