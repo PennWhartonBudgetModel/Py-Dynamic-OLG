605fae43f65e561ba78f51d83d1a3023a496f444
Pete <peterlu@wharton.upenn.edu>
2016-11-03 11:10:45 -0400

    Buffed up hardyload.
    Attempting to address file access issue on HPCC that may be due to sporadic I/O stutters on the cluster.

--- hardyload.m
+++ hardyload.m
@@ -6,20 +6,28 @@
 
 function [s] = hardyload(filename)
 
+% Turn on pause and store current pause state
+pause0 = pause('on');
+
 % Set maximum number of attempts and maximum pause time in seconds
-maxtries = 50;
-maxpause = 1.0;
+maxtries = 200;
+maxpause = 2.0;
 
 for itry = 1:maxtries
     try
+        % Attempt load
         s = load(filename);
         break
-    catch
+    catch e
         if (itry == maxtries)
-            error('Failed to load %s after %d attempts.', filename, maxtries)
+            error('Failed to load ''%s'' after %d attempts.\n%s', filename, maxtries, e.message)
         end
+        % Take a breather
         pause(rand * maxpause)
     end
 end
+
+% Reset pause state
+pause(pause0)
 
 end
