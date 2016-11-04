e41678d241a1bc3890efc494a36bf503675693c3
Pete <peterlu@wharton.upenn.edu>
2016-11-04 12:51:38 -0400

    Enabled parallelization across cohorts for the HPCC.
    Total number of compute nodes used simultaneously across jobs, tasks, and parallel pools should not exceed limit of 256.

--- generate_results.sh
+++ generate_results.sh
@@ -14,15 +14,20 @@ rm -rf ${LOGDIR}
 mkdir -p ${LOGDIR}
 
 
-# Submit closed economy runs
-# (Steady state and open economy runs executed as dependencies)
-for GCUT in +0.00 +0.10 +0.05 -0.05; do
-  for PLAN in base trump clinton ryan; do
+# Submit closed economy baseline runs
+# (Steady state and open economy baseline runs performed as dependencies)
+qsub -N baseline -t 1-16 -q short.q \
+		 -j y -o ${LOGDIR}'/closed_inddeep=$TASK_ID_base.log' \
+		 -b y 'matlab -nodesktop -nosplash -r "pool = parpool(12), solve_closed(inddeep_to_params(${SGE_TASK_ID}), '\''base'\'', +0.00, false), delete(pool)"'
+
+
+# Submit closed economy counterfactual runs
+# (Corresponding open economy counterfactual runs performed as dependencies)
+for GCUT in +0.10 +0.05 +0.00 -0.05; do
+  for PLAN in trump clinton ryan; do
     
-    # Skip non-zero gcut baseline runs
-    if [[ (${PLAN} == base) && (${GCUT} != +0.00) ]]; then continue; fi
-    
-    qsub -N solve_closed -t 1-16 -q short.q \
+		# (Note that parallelization across cohorts is disabled with -nojvm)
+    qsub -N counterfactual -hold_jid baseline -t 1-16 -q short.q \
          -j y -o ${LOGDIR}'/closed_inddeep=$TASK_ID_plan='${PLAN}'_gcut='${GCUT}'.log' \
          -b y 'matlab -nojvm -nosplash -r "solve_closed(inddeep_to_params(${SGE_TASK_ID}), '\'${PLAN}\'', '${GCUT}', false)"'
 		
@@ -31,7 +36,6 @@ done
 
 
 # Process and package results
-# (Note that Java is needed for package_results; hence the use of -nodesktop instead of -nojvm)
-qsub -N package -hold_jid solve_closed -q short.q \
+qsub -N package -hold_jid counterfactual -q short.q \
      -j y -o ${LOGDIR}'/package.log' \
-     -b y 'matlab -nodesktop -nosplash -r "check_closed_convergence, generate_static_aggregates_closed, package_results"'
+     -b y 'matlab -nodesktop -nosplash -r "pool = parpool(24), check_closed_convergence, generate_static_aggregates_closed, package_results, delete(pool)"'
