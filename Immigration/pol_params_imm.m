1fc33195ddbb4849ba023372cb342f5ddf565336
Pete <peterlu@wharton.upenn.edu>
2017-01-31 15:16:24 -0500

    Folded population_projector into pol_params_imm.
    Also performed basic cleanup.

--- Immigration/pol_params_imm.m
+++ Immigration/pol_params_imm.m
@@ -1,33 +1,16 @@
-function pol_params_imm(impolno)
-% impolno = 125;
-% 
-% iter = 0;
-% policy1 = zeros(5,5^3);
-% for run1 = 1:5
-%     for run2 = 1:5
-%         for run3 = 1:5
-%                     iter = iter + 1;                    
-%                     policy1(:,iter) = [run1, run2, run3,iter,iter + 0];
-%         end
-%     end
-% end
-% policy1 = policy1';
-% policy4 = policy1(:,4:5);
-% 
-% save polvals_imm.mat
+function [] = pol_params_imm()
 
 impolno = 1;
 jobdir = 'Testing';
 
-load polvals_imm.mat
-load params.mat Tss T z proddist_age
-load Imm_Data.mat
+load('polvals_imm.mat')
+load('params.mat', 'Tss', 'T', 'z', 'proddist_age')
+load('Imm_Data.mat')
 
 policy1 = policy1(impolno,:);
 
 
 % policy #1 increases the flow of legal immigrants
-
 if policy1(1)==1
     legal_rate = legal_rate.*1;
 elseif policy1(1)==2
@@ -39,6 +22,7 @@ elseif policy1(1)==4
 elseif policy1(1)==5
     legal_rate = legal_rate.*2;
 end
+
 
 % policy #2 increases the relative flow of skilled immigrants.
 % the idea here is to get the increase in avg productivity from someone and
@@ -65,7 +49,7 @@ end
 %  masses from low -> high or high -> low.  The problem is bounded by a max productivity,
 %  so not all productivity targets are feasible.
 
-for i1 = 2 % legal =2
+for i1 = 2
     for t1 = 1:T
         % We take the average over productivity shocks because the probability weights are uniform
         % (by 2-bin discretization of normal distributions)
@@ -95,6 +79,7 @@ for i1 = 2 % legal =2
     end
 end  
 
+
 % policy #3 grants amnesty to a percentage of illegal immigrants annually
 
 if policy1(3) ==1
@@ -113,6 +98,7 @@ elseif policy1(3)==5
     amnesty = .1;
     deportation = 0;
 end
+
 
 filename = ['imm_polparams_' num2str(impolno) '.mat'];
 save(fullfile(jobdir, filename))
@@ -147,5 +133,158 @@ end
 fprintf('\n');
 
 
+
+end
+
+
+
+
+function pop_trans = population_projector(impolno, jobdir)
+
+load('params.mat')
+load('Imm_Data.mat')
+
+filename = ['imm_polparams_' num2str(impolno) '.mat'];
+load(fullfile(jobdir, filename))
+
+
+Tr = NRA_baseline;
+
+load('SSVALS.mat', 'pop_prev')
+
+demtype = 1;  % by the symmetry of the demographic type sizes, population will grow equally on each "island" (i.e., demtype island).  WLOG, we choose demtype=1.
+filename = ['sspol' num2str(demtype) '.mat'];
+totfile = fullfile(jobdir,filename);
+load(totfile);
+
+filename = ['distvars_' num2str(demtype) '.mat'];
+totfile = fullfile(jobdir,filename);
+load(totfile, 'dist1ss', 'dist1ss_r');
+
+dist1ss_previous   = dist1ss  ;   % initiating working-age distribution array
+dist1ss_r_previous = dist1ss_r;   % initiating retiree distribution array
+
+pop = sum(sum(sum(sum(sum(dist1ss))))) + sum(sum(sum(sum(dist1ss_r))));    % initiating population
+
+pop_trans = zeros(Tss,1);    % fill in with transition population sizes
+pop_trans(1) = pop;
+
+
+for trans_year = 2:Tss
+    
+    fprintf('%u\n', trans_year);
+    
+    year = trans_year - 1;
+    
+    dist1ss = zeros(nk,nz,nb,T,3);   % last dimension is [native, legal, illegal]
+    dist1ss_r = zeros(nk,nb,T,3);
+    
+    % using period 1 imm rate values for steady state
+    im_flow = [ pop_trans(year) * pgr                          ;
+                pop_trans(year) * imm_age(1) * legal_rate(1)   ;
+                pop_trans(year) * imm_age(1) * illegal_rate(1) ];
+    
+    for i1=1:nz
+        dist1ss(1,i1,1,1,:) = squeeze(proddist_age(i1,1,:)).*(im_flow);
+    end
+    
+    for t1 = 1:Tr
+        
+        age = t1;
+        
+        im_flow = [ 0                                                ;
+                    pop_trans(year) * imm_age(age) * legal_rate(1)   ;
+                    pop_trans(year) * imm_age(age) * illegal_rate(1) ];
+        
+        for j2 = 1:nz
+            for i1 = 1:nk
+                for i2 = 1:nb
+                    
+                    point_k = max(kopt(i1,j2,i2,t1), kgrid(1));
+                    loc1 = find(kgrid(1:nk-1) <= point_k, 1, 'last');
+                    w1 = (point_k - kgrid(loc1)) / (kgrid(loc1+1) - kgrid(loc1));
+                    w1 = min(w1, 1);
+                    
+                    point_b = max(bopt(i1,j2,i2,t1), bgrid(1));
+                    loc2 = find(bgrid(1:nb-1) <= point_b, 1, 'last');
+                    w2 = (point_b - bgrid(loc2)) / (bgrid(loc2+1) - bgrid(loc2));
+                    w2 = min(w2, 1);
+                    
+                    for j4 = 1:nz
+                        
+                        if (t1 < Tr)
+                            
+                            for immigrant_type = 1:3
+                                
+                                % lack of generality in im flow indicator below
+                                dist_hold = squeeze(dist1ss_previous(i1,j2,i2,t1,immigrant_type)) + eq(i1,1)*eq(i2,1)*squeeze(proddist_age(j2,t1,immigrant_type)).*im_flow(immigrant_type);
+                                
+                                dist1ss(loc1,j4,loc2,t1+1,immigrant_type) = squeeze(dist1ss(loc1,j4,loc2,t1+1,immigrant_type)) + (surv(t1)*(1-w2)*(1-w1)*tr_z(j2,j4)).*dist_hold;
+                                dist1ss(loc1+1,j4,loc2,t1+1,immigrant_type) = squeeze(dist1ss(loc1+1,j4,loc2,t1+1,immigrant_type)) + (surv(t1)*(1-w2)*(w1)*tr_z(j2,j4)).*dist_hold;
+                                dist1ss(loc1,j4,loc2+1,t1+1,immigrant_type) = squeeze(dist1ss(loc1,j4,loc2+1,t1+1,immigrant_type)) + (surv(t1)*(w2)*(1-w1)*tr_z(j2,j4)).*dist_hold;
+                                dist1ss(loc1+1,j4,loc2+1,t1+1,immigrant_type) = squeeze(dist1ss(loc1+1,j4,loc2+1,t1+1,immigrant_type)) + (surv(t1)*(w2)*(w1)*tr_z(j2,j4)).*dist_hold;
+                                
+                            end
+                            
+                        else
+                            
+                            for immigrant_type = 1:3
+                                
+                                dist_hold = squeeze(dist1ss_previous(i1,j2,i2,t1,immigrant_type)) + eq(i1,1)*eq(i2,1)*squeeze(proddist_age(j2,t1,immigrant_type)).*im_flow(immigrant_type);
+                                
+                                dist1ss_r(loc1,loc2,t1+1,immigrant_type) = squeeze(dist1ss_r(loc1,loc2,t1+1,immigrant_type)) + (surv(t1)*(1-w2)*(1-w1)*tr_z(j2,j4)).*dist_hold;
+                                dist1ss_r(loc1+1,loc2,t1+1,immigrant_type) = squeeze(dist1ss_r(loc1+1,loc2,t1+1,immigrant_type)) + (surv(t1)*(1-w2)*(w1)*tr_z(j2,j4)).*dist_hold;
+                                dist1ss_r(loc1,loc2+1,t1+1,immigrant_type) = squeeze(dist1ss_r(loc1,loc2+1,t1+1,immigrant_type)) + (surv(t1)*(w2)*(1-w1)*tr_z(j2,j4)).*dist_hold;
+                                dist1ss_r(loc1+1,loc2+1,t1+1,immigrant_type) = squeeze(dist1ss_r(loc1+1,loc2+1,t1+1,immigrant_type)) + (surv(t1)*(w2)*(w1)*tr_z(j2,j4)).*dist_hold;
+                            
+                            end
+                            
+                        end
+                        
+                    end
+                    
+                end
+            end
+        end
+    end
+    
+    for t1 = Tr+1:T-1
+        
+        age = t1;
+        
+        im_flow = [0                                                ;
+                   pop_trans(year) * imm_age(age) * legal_rate(1)   ;
+                   pop_trans(year) * imm_age(age) * illegal_rate(1) ];
+        
+        for i1 = 1:nk
+            for i2 = 1:nb
+                
+                point_k = max(koptss(i1,i2,t1-Tr), kgrid(1));
+                loc1 = find(kgrid(1:nk-1) <= point_k, 1, 'last');
+                w1 = (point_k - kgrid(loc1)) / (kgrid(loc1+1) - kgrid(loc1));
+                w1 = min(w1, 1);
+                
+                for immigrant_type = 1:3
+                    
+                    dist_hold = squeeze(dist1ss_r_previous(i1,i2,t1,immigrant_type)) + eq(i1,1)*eq(i2,1)*im_flow(immigrant_type);
+                    
+                    dist1ss_r(loc1,i2,t1+1,immigrant_type) = squeeze(dist1ss_r(loc1,i2,t1+1,immigrant_type)) + (surv(t1)*(1-w1)).*dist_hold;
+                    dist1ss_r(loc1+1,i2,t1+1,immigrant_type) = squeeze(dist1ss_r(loc1+1,i2,t1+1,immigrant_type)) + (surv(t1).*w1)*dist_hold;
+                end
+                
+            end
+        end
+    end
+    
+    dist1ss(:,:,:,:,3) = (1-deportation).*dist1ss(:,:,:,:,3);  
+    dist1ss_r(:,:,:,3) = (1-deportation).*dist1ss_r(:,:,:,3);
+    
+    pop = sum(sum(sum(sum(sum(dist1ss))))) + sum(sum(sum(sum(dist1ss_r))));
+    pop_trans(trans_year) = pop;
+    
+    dist1ss_previous   = dist1ss  ;   % updating working-age distribution array
+    dist1ss_r_previous = dist1ss_r;   % updating retiree distribution array
+    
+end
 
 end
