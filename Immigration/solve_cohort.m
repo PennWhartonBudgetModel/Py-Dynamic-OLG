771b512bc6e02f9a164d1c7d8314ee9431042c7c
Pete <peterlu@wharton.upenn.edu>
2017-02-14 15:50:17 -0500

    Removed redundant demographic dimension from cohort solver.

--- Immigration/solve_cohort.m
+++ Immigration/solve_cohort.m
@@ -20,8 +20,8 @@ T_past   = max(-startyear, 0);
 T_shift  = max(+startyear, 0);
 T_active = min(startyear+T_life, T_model) - T_shift;
 
-dist_1 = zeros(nk,nz,nb,T_active,3,ndem);
-dist_r = zeros(nk,   nb,T_active,3,ndem);
+dist_1 = zeros(nk,nz,nb,T_active,3);
+dist_r = zeros(nk,   nb,T_active,3);
 
 
 opt = load(fullfile('Freeze', 'Cohorts', sprintf('cohort=%+03d_idem=%u.mat', startyear, idem)));
@@ -32,13 +32,12 @@ B   = opt.B  ;
 PIT = opt.PIT;
 SST = opt.SST;
 BEN = opt.BEN;
-SSB = opt.SSB;
 
 
 if (startyear < 0)
     
-    dist_1(:,:,:,1,:,idem) = dist1(:,:,:,T_past+1,:,idem); %#ok<NODEF>
-    dist_r(:,  :,1,:,idem) = distr(:,  :,T_past+1,:,idem); %#ok<NODEF>
+    dist_1(:,:,:,1,:) = dist1(:,:,:,T_past+1,:,idem); %#ok<NODEF>
+    dist_r(:,  :,1,:) = distr(:,  :,T_past+1,:,idem); %#ok<NODEF>
     
 else
     
@@ -51,7 +50,7 @@ else
     
     for iz = 1:nz
         for ipop = 1:3
-            dist_1(1,iz,1,1,ipop,idem) = proddist_age(iz,1,ipop) * im_flow(ipop);
+            dist_1(1,iz,1,1,ipop) = proddist_age(iz,1,ipop) * im_flow(ipop);
         end
     end
     
@@ -86,18 +85,18 @@ for t = 1:T_active-1
                     for jz = 1:nz
                         for ipop = 1:3
                             
-                            dist_hold = dist_1(ik,iz,ib,t,ipop,idem) + (ik == 1)*(ib == 1)*proddist_age(iz,age,ipop)*im_flow(ipop);
+                            dist_hold = dist_1(ik,iz,ib,t,ipop) + (ik == 1)*(ib == 1)*proddist_age(iz,age,ipop)*im_flow(ipop);
                             
                             if (age < T_work)
-                                dist_1(loc1  ,jz,loc2  ,t+1,ipop,idem) = dist_1(loc1  ,jz,loc2  ,t+1,ipop,idem) + surv(age)*(1-w2)*(1-w1)*tr_z(iz,jz)*dist_hold;
-                                dist_1(loc1+1,jz,loc2  ,t+1,ipop,idem) = dist_1(loc1+1,jz,loc2  ,t+1,ipop,idem) + surv(age)*(1-w2)*(w1  )*tr_z(iz,jz)*dist_hold;
-                                dist_1(loc1  ,jz,loc2+1,t+1,ipop,idem) = dist_1(loc1  ,jz,loc2+1,t+1,ipop,idem) + surv(age)*(w2  )*(1-w1)*tr_z(iz,jz)*dist_hold;
-                                dist_1(loc1+1,jz,loc2+1,t+1,ipop,idem) = dist_1(loc1+1,jz,loc2+1,t+1,ipop,idem) + surv(age)*(w2  )*(w1  )*tr_z(iz,jz)*dist_hold;
+                                dist_1(loc1  ,jz,loc2  ,t+1,ipop) = dist_1(loc1  ,jz,loc2  ,t+1,ipop) + surv(age)*(1-w2)*(1-w1)*tr_z(iz,jz)*dist_hold;
+                                dist_1(loc1+1,jz,loc2  ,t+1,ipop) = dist_1(loc1+1,jz,loc2  ,t+1,ipop) + surv(age)*(1-w2)*(w1  )*tr_z(iz,jz)*dist_hold;
+                                dist_1(loc1  ,jz,loc2+1,t+1,ipop) = dist_1(loc1  ,jz,loc2+1,t+1,ipop) + surv(age)*(w2  )*(1-w1)*tr_z(iz,jz)*dist_hold;
+                                dist_1(loc1+1,jz,loc2+1,t+1,ipop) = dist_1(loc1+1,jz,loc2+1,t+1,ipop) + surv(age)*(w2  )*(w1  )*tr_z(iz,jz)*dist_hold;
                             else
-                                dist_r(loc1  ,   loc2  ,t+1,ipop,idem) = dist_r(loc1  ,   loc2  ,t+1,ipop,idem) + surv(age)*(1-w2)*(1-w1)*tr_z(iz,jz)*dist_hold;
-                                dist_r(loc1+1,   loc2  ,t+1,ipop,idem) = dist_r(loc1+1,   loc2  ,t+1,ipop,idem) + surv(age)*(1-w2)*(w1  )*tr_z(iz,jz)*dist_hold;
-                                dist_r(loc1  ,   loc2+1,t+1,ipop,idem) = dist_r(loc1  ,   loc2+1,t+1,ipop,idem) + surv(age)*(w2  )*(1-w1)*tr_z(iz,jz)*dist_hold;
-                                dist_r(loc1+1,   loc2+1,t+1,ipop,idem) = dist_r(loc1+1,   loc2+1,t+1,ipop,idem) + surv(age)*(w2  )*(w1  )*tr_z(iz,jz)*dist_hold;
+                                dist_r(loc1  ,   loc2  ,t+1,ipop) = dist_r(loc1  ,   loc2  ,t+1,ipop) + surv(age)*(1-w2)*(1-w1)*tr_z(iz,jz)*dist_hold;
+                                dist_r(loc1+1,   loc2  ,t+1,ipop) = dist_r(loc1+1,   loc2  ,t+1,ipop) + surv(age)*(1-w2)*(w1  )*tr_z(iz,jz)*dist_hold;
+                                dist_r(loc1  ,   loc2+1,t+1,ipop) = dist_r(loc1  ,   loc2+1,t+1,ipop) + surv(age)*(w2  )*(1-w1)*tr_z(iz,jz)*dist_hold;
+                                dist_r(loc1+1,   loc2+1,t+1,ipop) = dist_r(loc1+1,   loc2+1,t+1,ipop) + surv(age)*(w2  )*(w1  )*tr_z(iz,jz)*dist_hold;
                             end
                             
                         end
@@ -109,32 +108,32 @@ for t = 1:T_active-1
         
         if (age < T_work)
             
-            amnesty_dist = squeeze(amnesty*dist_1(:,:,:,t+1,3,idem)); 
+            amnesty_dist = squeeze(amnesty*dist_1(:,:,:,t+1,3)); 
             amnesty_dist = permute(amnesty_dist, [2,1,3]);
             amnesty_dist = squeeze(sum(amnesty_dist));
             
-            prod_legal = squeeze(dist_1(:,:,:,t+1,2,idem));
+            prod_legal = squeeze(dist_1(:,:,:,t+1,2));
             prod_legal = squeeze(sum(sum(permute(prod_legal, [1,3,2]))));
             prod_legal = prod_legal / sum(prod_legal);
             
             for ik = 1:nk
                 for ib = 1:nb
                     for iz = 1:nz
-                        dist_1(ik,iz,ib,t+1,2,idem) = dist_1(ik,iz,ib,t+1,2,idem) + prod_legal(iz)*amnesty_dist(ik,ib);
+                        dist_1(ik,iz,ib,t+1,2) = dist_1(ik,iz,ib,t+1,2) + prod_legal(iz)*amnesty_dist(ik,ib);
                     end
                 end
             end
-            dist_1(:,:,:,t+1,3,idem) = (1-amnesty)*dist_1(:,:,:,t+1,3,idem);
+            dist_1(:,:,:,t+1,3) = (1-amnesty)*dist_1(:,:,:,t+1,3);
             
         else
             
-            dist_r(:,:,t+1,2,idem) = dist_r(:,:,t+1,2,idem) + amnesty*dist_r(:,:,t+1,3,idem);
-            dist_r(:,:,t+1,3,idem) = (1-amnesty)*dist_r(:,:,t+1,3,idem);
+            dist_r(:,:,t+1,2) = dist_r(:,:,t+1,2) + amnesty*dist_r(:,:,t+1,3);
+            dist_r(:,:,t+1,3) = (1-amnesty)*dist_r(:,:,t+1,3);
             
         end
         
-        dist_1(:,:,:,t+1,3,idem) = (1-deportation)*dist_1(:,:,:,t+1,3,idem);
-        dist_r(:,  :,t+1,3,idem) = (1-deportation)*dist_r(:,  :,t+1,3,idem);
+        dist_1(:,:,:,t+1,3) = (1-deportation)*dist_1(:,:,:,t+1,3);
+        dist_r(:,  :,t+1,3) = (1-deportation)*dist_r(:,  :,t+1,3);
         
     else
         
@@ -148,20 +147,20 @@ for t = 1:T_active-1
                 
                 for ipop = 1:3
                     
-                    dist_hold = dist_r(ik,ib,t,ipop,idem) + (ik == 1)*(ib == 1)*im_flow(ipop);
+                    dist_hold = dist_r(ik,ib,t,ipop) + (ik == 1)*(ib == 1)*im_flow(ipop);
                     
-                    dist_r(loc1  ,ib,t+1,ipop,idem) = dist_r(loc1  ,ib,t+1,ipop,idem) + surv(age)*(1-w1)*dist_hold;
-                    dist_r(loc1+1,ib,t+1,ipop,idem) = dist_r(loc1+1,ib,t+1,ipop,idem) + surv(age)*(w1  )*dist_hold;
+                    dist_r(loc1  ,ib,t+1,ipop) = dist_r(loc1  ,ib,t+1,ipop) + surv(age)*(1-w1)*dist_hold;
+                    dist_r(loc1+1,ib,t+1,ipop) = dist_r(loc1+1,ib,t+1,ipop) + surv(age)*(w1  )*dist_hold;
                     
                 end
                 
             end
         end
         
-        dist_r(:,:,t+1,2,idem) = dist_r(:,:,t+1,2,idem) + amnesty*dist_r(:,:,t+1,3,idem);
-        dist_r(:,:,t+1,3,idem) = (1-amnesty)*dist_r(:,:,t+1,3,idem);
+        dist_r(:,:,t+1,2) = dist_r(:,:,t+1,2) + amnesty*dist_r(:,:,t+1,3);
+        dist_r(:,:,t+1,3) = (1-amnesty)*dist_r(:,:,t+1,3);
         
-        dist_r(:,:,t+1,3,idem) = (1-deportation)*dist_r(:,:,t+1,3,idem);
+        dist_r(:,:,t+1,3) = (1-deportation)*dist_r(:,:,t+1,3);
         
     end
     
@@ -189,14 +188,14 @@ for t = 1:T_active
             for iz = 1:nz
                 for ik = 1:nk
                     for ib = 1:nb
-                        Cohort.assets (t) = Cohort.assets (t) + dist_1(ik,iz,ib,t,ipop,idem)*K  (ik,iz,ib,t)*(2-surv(age));
-                        Cohort.beqs   (t) = Cohort.beqs   (t) + dist_1(ik,iz,ib,t,ipop,idem)*K  (ik,iz,ib,t)*(1-surv(age));
-                        Cohort.labeffs(t) = Cohort.labeffs(t) + dist_1(ik,iz,ib,t,ipop,idem)*LAB(ik,iz,ib,t)*z(iz,age,idem);
-                        Cohort.labs   (t) = Cohort.labs   (t) + dist_1(ik,iz,ib,t,ipop,idem)*LAB(ik,iz,ib,t);
-                        Cohort.lfprs  (t) = Cohort.lfprs  (t) + dist_1(ik,iz,ib,t,ipop,idem)*(LAB(ik,iz,ib,t) > 0);
-                        Cohort.pits   (t) = Cohort.pits   (t) + dist_1(ik,iz,ib,t,ipop,idem)*PIT(ik,iz,ib,t);
-                        Cohort.ssts   (t) = Cohort.ssts   (t) + dist_1(ik,iz,ib,t,ipop,idem)*SST(ik,iz,ib,t);
-                        Cohort.bens   (t) = Cohort.bens   (t) + dist_1(ik,iz,ib,t,ipop,idem)*BEN(ik,iz,ib,t);
+                        Cohort.assets (t) = Cohort.assets (t) + dist_1(ik,iz,ib,t,ipop)*K  (ik,iz,ib,t)*(2-surv(age));
+                        Cohort.beqs   (t) = Cohort.beqs   (t) + dist_1(ik,iz,ib,t,ipop)*K  (ik,iz,ib,t)*(1-surv(age));
+                        Cohort.labeffs(t) = Cohort.labeffs(t) + dist_1(ik,iz,ib,t,ipop)*LAB(ik,iz,ib,t)*z(iz,age,idem);
+                        Cohort.labs   (t) = Cohort.labs   (t) + dist_1(ik,iz,ib,t,ipop)*LAB(ik,iz,ib,t);
+                        Cohort.lfprs  (t) = Cohort.lfprs  (t) + dist_1(ik,iz,ib,t,ipop)*(LAB(ik,iz,ib,t) > 0);
+                        Cohort.pits   (t) = Cohort.pits   (t) + dist_1(ik,iz,ib,t,ipop)*PIT(ik,iz,ib,t);
+                        Cohort.ssts   (t) = Cohort.ssts   (t) + dist_1(ik,iz,ib,t,ipop)*SST(ik,iz,ib,t);
+                        Cohort.bens   (t) = Cohort.bens   (t) + dist_1(ik,iz,ib,t,ipop)*BEN(ik,iz,ib,t);
                     end
                 end
             end
@@ -205,11 +204,11 @@ for t = 1:T_active
             
             for ik = 1:nk
                 for ib = 1:nb
-                    Cohort.assets(t) = Cohort.assets(t) + dist_r(ik,ib,t,ipop,idem)*K  (ik,1,ib,t)*(2-surv(age));
-                    Cohort.beqs  (t) = Cohort.beqs  (t) + dist_r(ik,ib,t,ipop,idem)*K  (ik,1,ib,t)*(1-surv(age));
-                    Cohort.pits  (t) = Cohort.pits  (t) + dist_r(ik,ib,t,ipop,idem)*PIT(ik,1,ib,t);
-                    Cohort.ssts  (t) = Cohort.ssts  (t) + dist_r(ik,ib,t,ipop,idem)*SST(ik,1,ib,t);
-                    Cohort.bens  (t) = Cohort.bens  (t) + dist_r(ik,ib,t,ipop,idem)*BEN(ik,1,ib,t);
+                    Cohort.assets(t) = Cohort.assets(t) + dist_r(ik,ib,t,ipop)*K  (ik,1,ib,t)*(2-surv(age));
+                    Cohort.beqs  (t) = Cohort.beqs  (t) + dist_r(ik,ib,t,ipop)*K  (ik,1,ib,t)*(1-surv(age));
+                    Cohort.pits  (t) = Cohort.pits  (t) + dist_r(ik,ib,t,ipop)*PIT(ik,1,ib,t);
+                    Cohort.ssts  (t) = Cohort.ssts  (t) + dist_r(ik,ib,t,ipop)*SST(ik,1,ib,t);
+                    Cohort.bens  (t) = Cohort.bens  (t) + dist_r(ik,ib,t,ipop)*BEN(ik,1,ib,t);
                 end
             end
             
