1a7e08bfbd3f7b23996fbc30c5d29dc20533915c
Pete <peterlu@wharton.upenn.edu>
2017-02-27 15:00:43 -0500

    Corrected parameter age indexing in transition path distribution generation.
    O(1%) changes to test targets.  Also renamed distribution variables.

--- Immigration/solve_trans.m
+++ Immigration/solve_trans.m
@@ -28,8 +28,7 @@ tr_z = s.tr_z;
 surv = s.surv;
 surv(T_life) = 0;
 
-pgr = zeros(1,T_life);
-pgr(1) = s.pgr;
+pgr = s.pgr;
 
 
 s = load('Imm_Data.mat');
@@ -39,6 +38,7 @@ imm_age      = s.imm_age;
 
 
 s = load(fullfile(jobdir, 'imm_polparams.mat'));
+
 proddist_age = s.proddist_age;
 legal_rate   = s.legal_rate;
 amnesty      = s.amnesty;
@@ -94,38 +94,34 @@ for idem = 1:ndem
     
     s = load(fullfile(jobdir, sprintf('distvars_%u.mat', idem)));
     
-    dist_previous     = s.dist;
-    dist_age_previous = ones(1,T_life);
+    DIST     = s.dist;
+    DIST_age = ones(1,T_life);
     
-    disteps = Inf;
-    disttol = -Inf;
-    
-    pops(1) = sum(s.dist(:));
+    DISTeps = Inf;
+    DISTtol = -Inf;
     
     year = 1;
     lastyear = T_model;
     
     
-    while (disteps > disttol && year <= lastyear)
+    while (DISTeps > DISTtol)
         
         fprintf('Year %3u\n', year);
         
-        
-        % --- Generate aggregates using dist_previous/dist0 ---
         
         for ik = 1:nk
             for iz = 1:nz
                 for ib = 1:nb
                     for age = 1:T_life
                         for ipop = 1:3
-                            Dynamic.assets (year) = Dynamic.assets (year) + dist_previous(ik,iz,ib,age,ipop)*K  (ik,iz,ib,age,year)*(2-surv(age));
-                            Dynamic.beqs   (year) = Dynamic.beqs   (year) + dist_previous(ik,iz,ib,age,ipop)*K  (ik,iz,ib,age,year)*(1-surv(age));
-                            Dynamic.labeffs(year) = Dynamic.labeffs(year) + dist_previous(ik,iz,ib,age,ipop)*LAB(ik,iz,ib,age,year)*z(iz,age,idem);
-                            Dynamic.labs   (year) = Dynamic.labs   (year) + dist_previous(ik,iz,ib,age,ipop)*LAB(ik,iz,ib,age,year);
-                            Dynamic.lfprs  (year) = Dynamic.lfprs  (year) + dist_previous(ik,iz,ib,age,ipop)*(LAB(ik,iz,ib,age,year) > 0);
-                            Dynamic.pits   (year) = Dynamic.pits   (year) + dist_previous(ik,iz,ib,age,ipop)*PIT(ik,iz,ib,age,year);
-                            Dynamic.ssts   (year) = Dynamic.ssts   (year) + dist_previous(ik,iz,ib,age,ipop)*SST(ik,iz,ib,age,year);
-                            Dynamic.bens   (year) = Dynamic.bens   (year) + dist_previous(ik,iz,ib,age,ipop)*BEN(ik,iz,ib,age,year);
+                            Dynamic.assets (year) = Dynamic.assets (year) + DIST(ik,iz,ib,age,ipop)*K  (ik,iz,ib,age,year)*(2-surv(age));
+                            Dynamic.beqs   (year) = Dynamic.beqs   (year) + DIST(ik,iz,ib,age,ipop)*K  (ik,iz,ib,age,year)*(1-surv(age));
+                            Dynamic.labeffs(year) = Dynamic.labeffs(year) + DIST(ik,iz,ib,age,ipop)*LAB(ik,iz,ib,age,year)*z(iz,age,idem);
+                            Dynamic.labs   (year) = Dynamic.labs   (year) + DIST(ik,iz,ib,age,ipop)*LAB(ik,iz,ib,age,year);
+                            Dynamic.lfprs  (year) = Dynamic.lfprs  (year) + DIST(ik,iz,ib,age,ipop)*(LAB(ik,iz,ib,age,year) > 0);
+                            Dynamic.pits   (year) = Dynamic.pits   (year) + DIST(ik,iz,ib,age,ipop)*PIT(ik,iz,ib,age,year);
+                            Dynamic.ssts   (year) = Dynamic.ssts   (year) + DIST(ik,iz,ib,age,ipop)*SST(ik,iz,ib,age,year);
+                            Dynamic.bens   (year) = Dynamic.bens   (year) + DIST(ik,iz,ib,age,ipop)*BEN(ik,iz,ib,age,year);
                         end
                     end
                 end
@@ -133,27 +129,29 @@ for idem = 1:ndem
         end
         
         
+        if (year == lastyear), break, else, year = year + 1; end
         
-        % --- Initialize distributions ---
         
-        dist = zeros(nk,nz,nb,T_life,3);
+        DIST_next = zeros(nk,nz,nb,T_life,3);
         
         
         for age = 1:T_life
             
-            im_flow = pops(year)*[ pgr(age); imm_age(max(age-1, 1))*legal_rate(1); imm_age(max(age-1, 1))*illegal_rate(1) ];
+            im_flow = sum(DIST(:))*[ (age == 1)*pgr               ;
+                                     imm_age(age)*legal_rate(1)   ;
+                                     imm_age(age)*illegal_rate(1) ];
             
             if (age == 1)
                 
                 for ipop = 1:3
-                    dist(1,:,1,age,ipop) = reshape(proddist_age(:,age,ipop)*im_flow(ipop), [1,nz,1,1,1]);
+                    DIST_next(1,:,1,age,ipop) = reshape(proddist_age(:,age,ipop)*im_flow(ipop), [1,nz,1,1,1]);
                 end
                 
             else
                 
                 % Extract optimal k and b values
-                k_t = max(K(:,:,:,age-1,year), ks(1));
-                b_t = max(B(:,:,:,age-1,year), bs(1));
+                k_t = max(K(:,:,:,age-1,year-1), ks(1));
+                b_t = max(B(:,:,:,age-1,year-1), bs(1));
                 
                 % Find indices of nearest values in ks and bs series
                 jk_lt = ones(size(k_t));
@@ -179,15 +177,15 @@ for idem = 1:ndem
                     for ipop = 1:3
                         
                         DIST_inflow = zeros(nk,nz,nb);
-                        DIST_inflow(1,:,1) = reshape(proddist_age(:,age-1,ipop)*im_flow(ipop), [1,nz,1]);
+                        DIST_inflow(1,:,1) = reshape(proddist_age(:,age,ipop)*im_flow(ipop), [1,nz,1]);
                         
-                        DIST_step = (dist_previous(:,:,:,age-1,ipop) + DIST_inflow) .* repmat(reshape(tr_z(:,jz), [1,nz,1]), [nk,1,nb]) * surv(age-1);
+                        DIST_step = (DIST(:,:,:,age-1,ipop)*surv(age-1) + DIST_inflow) .* repmat(reshape(tr_z(:,jz), [1,nz,1]), [nk,1,nb]);
                         
                         for elem = 1:numel(DIST_step)
-                            dist(jk_lt(elem), jz, jb_lt(elem), age, ipop) = dist(jk_lt(elem), jz, jb_lt(elem), age, ipop) + wk_lt(elem)*wb_lt(elem)*DIST_step(elem);
-                            dist(jk_gt(elem), jz, jb_lt(elem), age, ipop) = dist(jk_gt(elem), jz, jb_lt(elem), age, ipop) + wk_gt(elem)*wb_lt(elem)*DIST_step(elem);
-                            dist(jk_lt(elem), jz, jb_gt(elem), age, ipop) = dist(jk_lt(elem), jz, jb_gt(elem), age, ipop) + wk_lt(elem)*wb_gt(elem)*DIST_step(elem);
-                            dist(jk_gt(elem), jz, jb_gt(elem), age, ipop) = dist(jk_gt(elem), jz, jb_gt(elem), age, ipop) + wk_gt(elem)*wb_gt(elem)*DIST_step(elem);
+                            DIST_next(jk_lt(elem), jz, jb_lt(elem), age, ipop) = DIST_next(jk_lt(elem), jz, jb_lt(elem), age, ipop) + wk_lt(elem)*wb_lt(elem)*DIST_step(elem);
+                            DIST_next(jk_gt(elem), jz, jb_lt(elem), age, ipop) = DIST_next(jk_gt(elem), jz, jb_lt(elem), age, ipop) + wk_gt(elem)*wb_lt(elem)*DIST_step(elem);
+                            DIST_next(jk_lt(elem), jz, jb_gt(elem), age, ipop) = DIST_next(jk_lt(elem), jz, jb_gt(elem), age, ipop) + wk_lt(elem)*wb_gt(elem)*DIST_step(elem);
+                            DIST_next(jk_gt(elem), jz, jb_gt(elem), age, ipop) = DIST_next(jk_gt(elem), jz, jb_gt(elem), age, ipop) + wk_gt(elem)*wb_gt(elem)*DIST_step(elem);
                         end
                         
                     end
@@ -196,24 +194,21 @@ for idem = 1:ndem
             end
             
             % Increase legal immigrant population for amnesty, maintaining overall distribution over productivity levels
-            distz_legal = sum(sum(dist(:,:,:,age,2), 1), 3);
-            dist(:,:,:,age,2) = dist(:,:,:,age,2) + repmat(sum(amnesty*dist(:,:,:,age,3), 2), [1,nz,1]).*repmat(distz_legal, [nk,1,nb])/sum(distz_legal);
+            DISTz_legal = sum(sum(DIST_next(:,:,:,age,2), 1), 3);
+            DIST_next(:,:,:,age,2) = DIST_next(:,:,:,age,2) + repmat(sum(amnesty*DIST_next(:,:,:,age,3), 2), [1,nz,1]).*repmat(DISTz_legal, [nk,1,nb])/sum(DISTz_legal);
             
             % Reduce illegal immigrant population for amnesty and deportation
-            dist(:,:,:,age,3) = (1-amnesty-deportation)*dist(:,:,:,age,3);
+            DIST_next(:,:,:,age,3) = (1-amnesty-deportation)*DIST_next(:,:,:,age,3);
             
         end
         
-        dist_age = sum(sum(reshape(dist, [], T_life, 3), 1), 3);
-        disteps = max(abs(dist_age(2:end)/dist_age(1) - dist_age_previous(2:end)/dist_age_previous(1)));
-        dist_age_previous = dist_age;
+        DIST_age_next = sum(sum(reshape(DIST_next, [], T_life, 3), 1), 3);
+        DISTeps = max(abs(DIST_age_next(2:end)/DIST_age_next(1) - DIST_age(2:end)/DIST_age(1)));
         
-        pops(year+1) = sum(dist(:)); %#ok<AGROW>
-        dist_previous = dist;
-        year = year + 1;
+        DIST     = DIST_next;
+        DIST_age = DIST_age_next;
         
     end
-    
     
 end
 
