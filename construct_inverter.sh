41a4c94e6a9c13bca07da8be73ee94b5bfed4ddf
Pete <peterlu@wharton.upenn.edu>
2017-03-23 17:25:26 -0400

    Added extra job submission layer to address job sequencing issue.
    Batch initialization job needs to complete before batch solution array job is submitted so that number of batches can be counted and used for array job indexing.

--- construct_inverter.sh
+++ construct_inverter.sh
@@ -8,9 +8,9 @@
 qsub -N initialize_batches -q short.q -j y -o /dev/null -b y 'matlab -nojvm -nosplash -r "modelCalibrator.initialize_batches()"'
 
 
-# Submit batch solution array job, holding for batch initialization job
-qsub -N solve_batch -hold_jid initialize_batches -t 1-$(ls -1 ./Batches | wc -l) -q short.q -j y -o /dev/null -b y 'matlab -nojvm -nosplash -r "modelCalibrator.solve_batch(${SGE_TASK_ID})"'
+# Submit batch solution array job after batch initialization job completion
+qsub -N submitter -hold_jid initialize_batches -q short.q -j y -o /dev/null -b y $'qsub -N solve_batch -t 1-$(ls -1 ./Batches | wc -l) -q short.q -j y -o /dev/null -b y \'matlab -nojvm -nosplash -r \"modelCalibrator.solve_batch(${SGE_TASK_ID})\"\''
 
 
-# Submit inverter construction job, holding for batch solution array job
-qsub -N construct_inverter -hold_jid solve_batch -q short.q -j y -o /dev/null -b y 'matlab -nojvm -nosplash -r "modelCalibrator.construct_inverter(false)"'
+# Submit inverter construction job, holding for batch solution array job completion
+qsub -N construct_inverter -hold_jid submitter,solve_batch -q short.q -j y -o /dev/null -b y 'matlab -nojvm -nosplash -r "modelCalibrator.construct_inverter(false)"'
