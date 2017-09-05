6accb5f3fae6a7f0ca7d483d67eb9162f8e54e0f
Pete <peterlu@wharton.upenn.edu>
2017-09-05 14:14:57 -0400

    Adjusted production run coordination script for new AWS HPCC queue.

--- modelProducer.sh
+++ modelProducer.sh
@@ -12,12 +12,14 @@ rm -rf ${LOGDIR}
 mkdir -p ${LOGDIR}
 
 # Submit production run task array job
-qsub -N run -t 1-$(ls -1 ./Runs | wc -l) -q short.q \
+qsub -N run -t 1-$(ls -1 ./Runs | wc -l) \
+     -q aws-ppi.q -l aws_ppi -P bepp_ppi_aws \
      -j y -o ${LOGDIR}'/run$TASK_ID.log' \
      -b y 'matlab -nojvm -nosplash -r "modelProducer.run(${SGE_TASK_ID})"'
 
 # Submit results packaging job, holding for production run task array job
 # (Note use of -nodesktop instead of -nojvm to activate Java for package_results method)
-qsub -N package_results -hold_jid run -q short.q \
+qsub -N package_results -hold_jid run \
+     -q short.q \
      -j y -o ${LOGDIR}'/package_results.log' \
      -b y 'matlab -nodesktop -nosplash -r "modelProducer.check_terminations(), modelProducer.package_results()"'
