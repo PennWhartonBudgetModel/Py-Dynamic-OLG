661d00525c3a6b04a80f152b53205f65f7754b55
Pete <peterlu@wharton.upenn.edu>
2017-09-06 11:27:25 -0400

    Adjusted calibration run coordination script for new AWS HPCC queue.
    Also updated modelCalibrator discretization level to test AWS parallelization.

--- modelCalibrator.sh
+++ modelCalibrator.sh
@@ -12,11 +12,13 @@ rm -rf ${LOGDIR}
 mkdir -p ${LOGDIR}
 
 # Submit batch solution task array job
-qsub -N solve_batch -t 1-$(ls -1 ./Batches | wc -l) -q short.q \
-     -j y -o ${LOGDIR}'/run$TASK_ID.log'  \
+qsub -N solve_batch -t 1-$(ls -1 ./Batches | wc -l) \
+     -q aws-ppi.q -l aws_ppi -P bepp_ppi_aws \
+     -j y -o ${LOGDIR}'/batch$TASK_ID.log'  \
      -b y 'matlab -nojvm -nosplash -r "modelCalibrator.solve_batch(${SGE_TASK_ID})"'
 
 # Submit batch consolidation job, holding for batch solution task array job
-qsub -N consolidate_batches -hold_jid solve_batch -q short.q \
+qsub -N consolidate_batches -hold_jid solve_batch \
+     -q short.q \
      -j y -o /dev/null \
      -b y 'matlab -nojvm -nosplash -r "modelCalibrator.consolidate_batches(false)"'
