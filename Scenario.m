5043244c5b878d23f41b80c63a700ccb5c721ae0
Pete <peterlu@wharton.upenn.edu>
2017-09-29 17:07:56 -0400

    Adjusted Scenario code cosmetics.
    Also condensed getParams implementation.

--- Scenario.m
+++ Scenario.m
@@ -27,23 +27,30 @@ classdef Scenario
     properties (Constant, Access = private )
     
         % Define list of required fields
-        req_fields = { 'economy', 'beta', 'sigma', 'gamma', 'modelunit_dollar', 'bequest_phi_1' };
+        req_fields = {...
+            'economy'           ;
+            'beta'              ;
+            'sigma'             ;
+            'gamma'             ;
+            'modelunit_dollar'  ;
+            'bequest_phi_1'     };
         
         % Specify default values for optional fields
-        def_fields = struct(    'taxplan'           , 'base'    ...
-                            ,   'gcut'              , 0.0       ...
-                            ,   'legal_scale'       , 1.0       ...
-                            ,   'prem_legal'        , 1.0       ...
-                            ,   'amnesty'           , 0.0       ...
-                            ,   'deportation'       , 0.0       ...
-                            );
+        def_fields = struct(...
+            'taxplan'       , 'base'    , ...
+            'gcut'          , 0.0       , ...
+            'legal_scale'   , 1.0       , ...
+            'prem_legal'    , 1.0       , ...
+            'amnesty'       , 0.0       , ...
+            'deportation'   , 0.0       );
+        
     end
     
     
     methods
         
         % Constructor
-        function this = Scenario(props)
+        function [this] = Scenario(props)
             
             if( ~isa( props, 'struct' ) )
                 error( 'Scenario constructor expects structure of initial variables.' );
@@ -77,7 +84,7 @@ classdef Scenario
         
         
         % Derived property: Get if isCurrentPolicy
-        function flag = isCurrentPolicy(this)
+        function [flag] = isCurrentPolicy(this)
              % isCurrentPolicy if no deviation from def_fields  
             for f = fieldnames(Scenario.def_fields)'
                 this_one = (this.(f{1})); def_one = (Scenario.def_fields.(f{1}));
@@ -98,79 +105,72 @@ classdef Scenario
         end
         
         
-        % Return cloned Scenario with override to current policy
-        function obj = currentPolicy(this)
-            obj = Scenario(this.getParams);
+        % Generate analogous current policy scenario
+        function [scenario] = currentPolicy(this)
+            scenario = Scenario(this.getParams());
             for f = fieldnames(Scenario.def_fields)'
-                obj.(f{1}) = Scenario.def_fields.(f{1});
+                scenario.(f{1}) = Scenario.def_fields.(f{1});
             end
-        end % currentPolicy
+        end
         
         
-        % Return cloned Scenario with 'steady' economy
-        function obj = steady(this)
+        % Generate analogous steady state scenario
+        function [scenario] = steady(this)
             params          = this.getParams();
             params.economy  = 'steady';
-            obj             = Scenario(params);
-        end % steady
+            scenario        = Scenario(params);
+        end
         
         
-        % Return cloned Scenario with 'open' economy
-        function obj = open(this)
+        % Generate analogous open economy scenario
+        function [scenario] = open(this)
             params          = this.getParams();
             params.economy  = 'open';
-            obj             = Scenario(params);
-        end % open
+            scenario        = Scenario(params);
+        end
         
         
-        % Return cloned Scenario with 'closed' economy
-        function obj = closed(this)
+        % Generate analogous closed economy scenario
+        function [scenario] = closed(this)
             params          = this.getParams();
             params.economy  = 'closed';
-            obj             = Scenario(params);
-        end % open
+            scenario        = Scenario(params);
+        end
         
         
         % Generate tags for baseline and counterfactual definitions
-        %     NOTE: These tags are not guaranteed to be unique across
-        %     Scenarios. These are for Development. For Production, we use
-        %     the Scenario.ID
         function [basedef_tag, counterdef_tag] = generate_tags(this)
 
-            basedef_tag = [     sprintf('%.3f' , this.beta)             ...
-                            ,   sprintf('_%.3f', this.gamma)            ...     
-                            ,   sprintf('_%.2f', this.sigma)            ...
-                            ,   sprintf('_%e'  , this.modelunit_dollar) ...
-                            ,   sprintf('_%.3f', this.bequest_phi_1)    ...
-                          ];
+            basedef_tag = [...
+                sprintf('%.3f'  , this.beta             ),...
+                sprintf('_%.3f' , this.gamma            ), ...
+                sprintf('_%.2f' , this.sigma            ), ...
+                sprintf('_%e'   , this.modelunit_dollar ), ...
+                sprintf('_%.3f' , this.bequest_phi_1    )];
             
             if( this.isCurrentPolicy )
                 counterdef_tag = 'baseline';
             else
-                counterdef_tag  = [     sprintf('%s'    , this.taxplan)        ...
-                                    ,   sprintf('_%+.2f', this.gcut)           ...
-                                    ,   sprintf('_%.1f' , this.legal_scale)    ...
-                                    ,   sprintf('_%.3f' , this.prem_legal)     ...
-                                    ,   sprintf('_%.2f' , this.amnesty)        ...
-                                    ,   sprintf('_%.2f' , this.deportation)    ...
-                                  ];
+                counterdef_tag = [...
+                    sprintf('%s'    , this.taxplan      ), ...
+                    sprintf('_%+.2f', this.gcut         ), ...
+                    sprintf('_%.1f' , this.legal_scale  ), ...
+                    sprintf('_%.3f' , this.prem_legal   ), ...
+                    sprintf('_%.2f' , this.amnesty      ), ...
+                    sprintf('_%.2f' , this.deportation  )];
             end
-        end % generate_tags
+        end
 
 
-        function params = getParams(this)
+        function [params] = getParams(this)
             params = struct();
-            % Create and copy all fields
-            for f = Scenario.req_fields
+            for f = [ Scenario.req_fields ; fieldnames(Scenario.def_fields) ]'
                 params.(f{1}) = this.(f{1});
             end
-            for f = fieldnames(Scenario.def_fields)'
-                params.(f{1}) = this.(f{1});
-            end
-        end % getParams
+        end
         
-    end % instance methods
+    end
     
     
-end % Scenario
+end
 
