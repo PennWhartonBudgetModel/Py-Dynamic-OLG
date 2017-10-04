9b2799b8925406d715aa5310507a8562ade5ec97
Pete <peterlu@wharton.upenn.edu>
2017-10-04 17:34:51 -0400

    Removed grouping of calibration points into execution batches.
    No longer necessary given smaller calibration grids that fit within HPCC task array job size limit.

--- ModelCalibrator.sh
+++ ModelCalibrator.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
+#$ -N define
 #$ -q short.q
 #$ -j y
 #$ -o /dev/null
 
-# Build mex functions and define batches
-matlab -nodesktop -nosplash -r "PathFinder.setToProductionMode(), MexBuilder.all(), ModelCalibrator.define_batches()"
+# Build mex functions and define calibration points
+matlab -nodesktop -nosplash -r "PathFinder.setToProductionMode(), MexBuilder.all(), ModelCalibrator.definePoints()"
 
 # Clear or create log directory
 LOGDIR='./Logs'
 rm -rf ${LOGDIR}
 mkdir -p ${LOGDIR}
 
-# Specify batch solution job execution queue, checking for presence of --aws flag
+# Specify calibration job execution queue, checking for presence of --aws flag ($1)
 QUEUE=$([ $# -gt 0 ] && [ $1 = "--aws" ]               \
         && echo 'aws-ppi.q -l aws_ppi -P bepp_ppi_aws' \
         || echo 'short.q'                              )
 
-# Submit batch solution task array job
-# (Note use of -nojvm to deactivate Java and hence deactivate Matlab parfor)
-qsub -N solve_batch -t 1-$(ls -1 ./Batches | wc -l) \
+# Submit calibration task array job
+#   Note use of -nojvm to deactivate Java and hence deactivate Matlab parfor
+qsub -N calibrate -t 1-$(ls -1 ./CalibrationPoints | wc -l) \
      -q ${QUEUE} \
-     -j y -o ${LOGDIR}'/batch$TASK_ID.log'  \
-     -b y 'matlab -nojvm -nosplash -r "PathFinder.setToProductionMode(), ModelCalibrator.solve_batch(${SGE_TASK_ID})"'
+     -j y -o ${LOGDIR}'/point$TASK_ID.log'  \
+     -b y 'matlab -nojvm -nosplash -r "PathFinder.setToProductionMode(), ModelCalibrator.calibratePoint(${SGE_TASK_ID})"'
 
-# Submit batch consolidation job, holding for batch solution task array job
-qsub -N consolidate_batches -hold_jid solve_batch \
+# Submit calibration point consolidation job, holding for calibration task array job
+qsub -N consolidate -hold_jid calibrate \
      -q short.q \
-     -j y -o /dev/null \
-     -b y 'matlab -nodesktop -nosplash -r "PathFinder.setToProductionMode(), ModelCalibrator.consolidate_batches(false)"'
+     -j y -o ${LOGDIR}'/consolidate.log' \
+     -b y 'matlab -nodesktop -nosplash -r "PathFinder.setToProductionMode(), ModelCalibrator.consolidatePoints()"'
