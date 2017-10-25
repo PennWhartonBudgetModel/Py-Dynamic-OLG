c194c52bc629a500c528e8e8d8d953d8d9ca82a7
Daniela <danielav@wharton.upenn.edu>
2017-10-25 12:47:32 -0400

    Corrected revenues calculation for the static economy. This should be a temporary fix.

--- ModelSolver.m
+++ ModelSolver.m
@@ -348,11 +348,13 @@ methods (Static)
                 Static.(series{1}) = Static_.(series{1});
             end
             
+            % THIS CALCULATION OF REVS SHOULD BE TEMPORARY
+            Static.revs  = Static.pits + Static.ssts + Static.cits - Static.bens;
             
             % Copy additional static aggregates from baseline aggregates
             Dynamic_base = hardyload('dynamics.mat', base_generator, base_dir);
             
-            for series = {'labeffs', 'caps', 'lfprs', 'labincs', 'capincs', 'outs', 'caps_domestic', 'caps_foreign', 'debts_domestic', 'debts_foreign', 'debts', 'revs', 'assets'}
+            for series = {'labeffs', 'caps', 'lfprs', 'labincs', 'capincs', 'outs', 'caps_domestic', 'caps_foreign', 'debts_domestic', 'debts_foreign', 'debts', 'assets'}
                 Static.(series{1}) = Dynamic_base.(series{1});
             end
             
