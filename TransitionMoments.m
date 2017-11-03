22c2a627513bef41429b6db8c49932ccc96babe7
Daniela <danielav@wharton.upenn.edu>
2017-11-03 16:20:58 -0400

    Allowed for static and counterfactual quintiles to be generated in a single run. Cleaned code and created functions for repetitive routines.

--- TransitionMoments.m
+++ TransitionMoments.m
@@ -1,9 +1,9 @@
 %%
-% Distribution moments generator tool.
-%
+% Generate moments distribution for transition path
+% 
 %%
 
-%% SCENARIOS
+%% Scenarios and directories
 
 % Params for K/Y=3 with d=0.056
 % params.beta =  0.98745000;
@@ -24,171 +24,195 @@ scenario   = Scenario(struct('economy', 'closed', 'beta', params.beta, 'gamma', 
                              'sigma', params.sigma, 'modelunit_dollar', params.modelunit_dollar, ...
                              'depreciation', params.depreciation, 'bequest_phi_1', 0, 'base_brackets', 'C'));
 
-% Scenarios and directories
 sc_steady  = scenario.currentPolicy.steady;
-sc_open    = scenario.open();
-sc_closed  = scenario.closed();
-sc_obase   = scenario.currentPolicy.open;
-sc_cbase   = scenario.currentPolicy.closed;
 steady_dir = PathFinder.getWorkingDir(sc_steady);
+sc_dir     = PathFinder.getWorkingDir(scenario);
 
-sc_use  = sc_open;
-use_dir = PathFinder.getWorkingDir(sc_use);
-sc_base = sc_obase;
+if scenario.economy == 'closed'
+    sc_base = scenario.currentPolicy.closed;
+else
+    sc_base = scenario.currentPolicy.open;
+end
 base_dir = PathFinder.getWorkingDir(sc_base);
 
 
 %% PARAMETERS
 
 % Define time constants
-s = ParamGenerator.timing( sc_use );
+s = ParamGenerator.timing( scenario );
 T_life  = s.T_life;    % Total life years
 T_work  = s.T_work;    % Retirement age
 T_model = s.T_model;   % Transition path model years
 
 % Define grids
-s = ParamGenerator.grids( sc_use );
-ndem = s.ndem;       % demographic types
-ng   = s.ng;         % num groups
-nz   = s.nz;         % num labor productivity shocks
-zs   = s.zs;         % shocks grid (by demographic type and age)
-nk   = s.nk;         % num asset points
-nb   = s.nb;         % num avg. earnings points
-% Useful later for a couple of functions
-kv = s.kv;
-karray = repmat(reshape(kv, [1,nk,1,1,1,1,1]),[nz,1,nb,T_life,ng,T_model+1,ndem]);
-yearsv = [2017:2017+T_model];
+s = ParamGenerator.grids( scenario );
+ndem   = s.ndem;       % demographic types
+ng     = s.ng;         % num groups
+nz     = s.nz;         % num labor productivity shocks
+zs     = s.zs;         % shocks grid (by demographic type and age)
+nk     = s.nk;         % num asset points
+nb     = s.nb;         % num avg. earnings points
+kv     = s.kv;         % asset grid
 
 
-%% DISTRIBUTION AND POLICY FUNCTIONS
+%% Households distribution
 
-% Import households distribution
-DIST   = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
-s      = load( fullfile(steady_dir, 'distribution.mat' ) );
-DISTss = s.DIST;
-% s      = load( fullfile(use_dir, 'distribution.mat' ) );
-s      = load( fullfile(use_dir, 'Static_distribution.mat' ) );
-DIST(:,:,:,:,:,1    ,:) = DISTss;
-% DIST(:,:,:,:,:,2:end,:) = s.DIST;
-DIST(:,:,:,:,:,2:end,:) = s.Static_DIST;
+[dist dist_static] = append_dist(nz, nk, nb, T_life, ng, T_model, ndem, steady_dir, sc_dir);
 
-% Import market variables
-s       = load( fullfile(steady_dir, 'market.mat' ) );
-wagesss = s.wages;
-s       = load( fullfile(base_dir, 'market.mat' ) );
-wages   = [wagesss s.wages];
+%% Import taxable income and define quintiles
 
-% Import policy functions
-labinc = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
-lab    = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
-pit    = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
-cit    = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
-inc    = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
-sst    = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
+[inc inc_static] = append_decisions('INC', nz, nk, nb, T_life, ng, T_model, ndem, kv, steady_dir, sc_dir);
 
-f = @(X) repmat(reshape(X, [nz,nk,nb,T_life,1,1,ndem]), [1,1,1,1,ng,1,1]);
-s = load( fullfile(steady_dir, 'all_decisions.mat' ) );
-lab(:,:,:,:,:,1,:) = f(s.LAB);
-pit(:,:,:,:,:,1,:) = f(s.PIT);
-cit(:,:,:,:,:,1,:) = f(s.CIT);
-inc(:,:,:,:,:,1,:) = f(s.INC);
-sst(:,:,:,:,:,1,:) = f(s.SST);
+inc_groups = zeros(T_model+1,5);
+inc_index  = zeros(T_model+1,5);
+sort_inc   = zeros(nz*nk*nb*T_life*ng*ndem, T_model+1);
+inc_groups_static = zeros(T_model+1,5);
+inc_index_static  = zeros(T_model+1,5);
+sort_inc_static   = zeros(nz*nk*nb*T_life*ng*ndem, T_model+1);
 
-f = @(X) repmat(reshape(X, [nz,nk,nb,T_life,1,T_model,ndem]), [1,1,1,1,ng,1,1]);
-% s = load( fullfile(use_dir, 'all_decisions.mat' ) );
-s = load( fullfile(use_dir, 'Static_all_decisions.mat' ) );
-lab(:,:,:,:,:,2:end,:) = f(s.LAB);
-pit(:,:,:,:,:,2:end,:) = f(s.PIT);
-cit(:,:,:,:,:,2:end,:) = f(s.CIT);
-inc(:,:,:,:,:,2:end,:) = f(s.INC);
-sst(:,:,:,:,:,2:end,:) = f(s.SST);
+[inc_groups sort_inc inc_index] = get_quintiles(inc, dist, sort_inc, inc_index, T_model);
+inc_groups = params.modelunit_dollar * inc_groups;
 
-labinc = lab .* repmat(reshape(zs, [nz,1,1,T_life,1,1,ndem]),[1,nk,nb,1,ng,T_model+1,1]) .* repmat(reshape(wages, [1,1,1,1,1,T_model+1,1]),[nz,nk,nb,T_life,ng,1,ndem]);
-taxes1 = pit + cit;
-taxes2 = pit + cit + sst;
+[inc_groups_static sort_inc_static inc_index_static] = get_quintiles(inc_static, dist_static, sort_inc, inc_index, T_model);
+inc_groups_static = params.modelunit_dollar * inc_groups_static;
 
 
-%% CIT TAXES DISTRIBUTION BY TAXABLE INCOME
+%% Generate quintiles for other variables based on taxable income distribution
 
-inc_dist   = zeros(5,1);
-inc_cum    = zeros(5,1);
-inc_share  = zeros(5,1);
-inc_groups = zeros(5,1);
-asset_cum  = zeros(5,1);
-asset_groups = zeros(5,1);
-cit_cum    = zeros(5,1);
-cit_groups = zeros(5,1);
-pit_cum    = zeros(5,1);
-pit_groups = zeros(5,1);
-linc_cum    = zeros(5,1);
-linc_groups = zeros(5,1);
-
-% Period variables (steady state = 1)
-t = 2;
-inc_1   = inc(:,:,:,:,:,t,:);
-dist_1  = DIST(:,:,:,:,:,t,:);
-asset_1 = karray(:,:,:,:,:,t,:);
-cit_1   = cit(:,:,:,:,:,t,:);
-pit_1   = pit(:,:,:,:,:,t,:);
-linc_1  = labinc(:,:,:,:,:,t,:);
-
-% Vectorize variables
-inc_1   = inc_1(:);
-dist_1  = dist_1(:);
-asset_1 = asset_1(:);
-cit_1   = cit_1(:);
-pit_1   = pit_1(:);
-linc_1  = linc_1(:);
-
-% Sort variables
-[inc_1, sortv] = sort(inc_1);
-dist_1  = dist_1(sortv);
-asset_1 = asset_1(sortv);
-cit_1   = cit_1(sortv);
-pit_1   = pit_1(sortv);
-linc_1  = linc_1(sortv);
-
-% Find quintiles
-q = 1;
-total_inc = sum(inc_1.*dist_1);
-for quintile = 0.2:0.2:0.8
-    
-    % Taxable income distribution
-    i = find(cumsum(dist_1) >= quintile,1);
-    inc_dist(q,1)  = sum(dist_1(1:i));
-    inc_cum(q,1)   = sum(inc_1(1:i).*dist_1(1:i));
-    inc_share(q,1) = inc_cum(q,1)/total_inc;
-    
-    % Asset distributed according to taxable income
-    asset_cum(q,1) = sum(asset_1(1:i).*dist_1(1:i));
-    % Labor income distributed according to taxable income
-    linc_cum(q,1) = sum(linc_1(1:i).*dist_1(1:i));
-    
-    % Taxes distributed according to taxable income
-    cit_cum(q,1) = sum(cit_1(1:i).*dist_1(1:i));
-    pit_cum(q,1) = sum(pit_1(1:i).*dist_1(1:i));
-    
-    % Counter
-    q = q + 1;
+var_list = {'CIT', 'K', 'PIT'};
+for var_name = var_list
+        
+    [var var_static] = append_decisions(var_name{1}, nz, nk, nb, T_life, ng, T_model, ndem, kv, steady_dir, sc_dir);
+    quintiles.(var_name{1}) = generate_moments(var, dist, sort_inc, inc_index, T_model);
+    quintiles.(strcat(var_name{1},'_static')) = generate_moments(var_static, dist_static, sort_inc_static, inc_index_static, T_model);
     
 end
 
-% Top quintile
-% Taxable income
-inc_dist(q,1)  = sum(dist_1);
-inc_cum(q,1)   = total_inc;
-inc_share(q,1) = 1;
-inc_groups = params.modelunit_dollar * diff([0 inc_cum'])';
-% Assets
-asset_cum(q,1) = sum(asset_1(1:end).*dist_1(1:end));
-asset_groups = diff([0 asset_cum'])';
-% Labor income
-linc_cum(q,1) = sum(linc_1(1:end).*dist_1(1:end));
-linc_groups = diff([0 linc_cum'])';
-% Taxes
-cit_cum(q,1) = sum(cit_1(1:end).*dist_1(1:end));
-cit_groups = diff([0 cit_cum'])';
-pit_cum(q,1) = sum(pit_1(1:end).*dist_1(1:end));
-pit_groups = diff([0 pit_cum'])';
+save_dir = PathFinder.getWorkingDir(scenario);
+save(fullfile(save_dir, 'quintiles.mat' ), '-struct', 'quintiles')
 
 
+%% FUNCTIONS
+
+% Pre-appends steady state distribution
+function [x x_static] = append_dist(nz, nk, nb, T_life, ng, T_model, ndem, dir_ss, dir_sc)
+
+    x = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
+    x_static = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
+
+    s = load( fullfile(dir_ss, 'distribution.mat' ) );
+    x       (:,:,:,:,:,1,:) = s.DIST;
+    x_static(:,:,:,:,:,1,:) = s.DIST;
+
+    s = load( fullfile(dir_sc, 'distribution.mat' ) );
+    x(:,:,:,:,:,2:end,:) = s.DIST;
+    s = load( fullfile(dir_sc, 'Static_distribution.mat' ) );
+    x_static(:,:,:,:,:,2:end,:) = s.Static_DIST;
+        
+end
+
+% Pre-appends steady state variables from all_decisions mat file
+function [x x_static] = append_decisions(x_name, nz, nk, nb, T_life, ng, T_model, ndem, kv, dir_ss, dir_sc)
+
+	if x_name == 'K'
+        
+        x = repmat(reshape(kv, [1,nk,1,1,1,1,1]),[nz,1,nb,T_life,ng,T_model+1,ndem]);
+        x_static = repmat(reshape(kv, [1,nk,1,1,1,1,1]),[nz,1,nb,T_life,ng,T_model+1,ndem]);
+        
+    else
+        
+        x = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
+        x_static = zeros(nz,nk,nb,T_life,ng,T_model+1,ndem);
+        f = @(X) repmat(reshape(X, [nz,nk,nb,T_life,1,1,ndem]), [1,1,1,1,ng,1,1]);
+        s = load( fullfile(dir_ss, 'all_decisions.mat' ) );
+        x(:,:,:,:,:,1,:) = f(s.(x_name));
+        x_static(:,:,:,:,:,1,:) = f(s.(x_name));
+        f = @(X) repmat(reshape(X, [nz,nk,nb,T_life,1,T_model,ndem]), [1,1,1,1,ng,1,1]);
+        s = load( fullfile(dir_sc, 'all_decisions.mat' ) );
+        x(:,:,:,:,:,2:end,:) = f(s.(x_name));
+        s = load( fullfile(dir_sc, 'Static_all_decisions.mat' ) );
+        x_static(:,:,:,:,:,2:end,:) = f(s.(x_name));
+        
+    end
+
+end
+
+
+% Get quintiles function
+
+function [quintiles sort_x index_x] = get_quintiles(x, dist, sort_x, index_x, T_model)
+
+    for t = 1:T_model+1
+
+    % Vectorize variables
+    x_t    = x(:,:,:,:,:,t,:);
+    x_t    = x_t(:);
+    dist_t = dist(:,:,:,:,:,t,:);
+    dist_t = dist_t(:);
+
+    % Sort variables
+    [x_t, sort_x(:,t)] = sort(x_t);
+    dist_t = dist_t(sort_x(:,t));
+
+    % Find quintiles
+    x_cum = zeros(1,5);
+    q = 1;
+    for quintile = 0.2:0.2:0.8
+
+        % Taxable income distribution
+        i = find(cumsum(dist_t) >= quintile,1);
+        x_cum(1,q)   = sum(x_t(1:i).*dist_t(1:i));
+        index_x(t,q) = i;
+
+        % Counter
+        q = q + 1;
+
+    end
+
+    % Top quintile
+    x_cum(1,q)   = sum(x_t.*dist_t);
+	index_x(t,q) = size(x_t,1);
+    % Groups
+    quintiles(t,:) = diff([0 x_cum(1,:)]);
+
+end
+
+end
+
+% generate_moments function
+function [x_groups] = generate_moments(x, dist, sortx, q_index, T_model)
+
+    x_groups = zeros(T_model+1,5);
+
+    for t = 1:T_model+1
+
+        % Vectorize variables
+        x_t    = x(:,:,:,:,:,t,:);
+        dist_t = dist(:,:,:,:,:,t,:);
+        x_t    = x_t(:);
+        dist_t = dist_t(:);
+
+        % Sort variables
+        x_t    = x_t(sortx(:,t));
+        dist_t = dist_t(sortx(:,t));
+
+        % Find quintiles
+        x_cum = zeros(1,5);
+        q = 1;
+        for quintile = 0.2:0.2:0.8
+
+            % Asset distributed according to taxable income
+            i = q_index(t,q);
+            x_cum(1,q) = sum(x_t(1:i).*dist_t(1:i));
+            % Counter
+            q = q + 1;
+
+        end
+
+        % Top quintile
+        x_cum(1,q)    = sum(x_t(1:end).*dist_t(1:end));
+        x_groups(t,:) = diff([0 x_cum(1,:)]);
+
+    end
+
+end
