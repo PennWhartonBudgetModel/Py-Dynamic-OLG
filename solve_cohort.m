d2529c16775d2a4991f106a99a52043fb5b12a2f
Daniela <danielav@wharton.upenn.edu>
2017-12-13 17:42:37 -0500

    Updated calculate_b function to account for a floor income.
    ModelTester full match. However if b is off-grid the mex file will prevent us to detect errors and deviations will be huge. Line 500 is crucial.

--- solve_cohort.m
+++ solve_cohort.m
@@ -493,8 +493,11 @@ if (nargin > 1)
 end
 
 
-% Calculate average earnings and caps it to the maximum taxable earnings - bv(end)
-b = min(bv_nb, (bv_ib*(age-1) + min(labinc, ssincmax))/age);
+% Calculate average earnings and cap it to the minimum (floor) and maximum (ceiling) taxable earnings
+b = max(ssincmin, (bv_ib*(age-1) + min(labinc, ssincmax))/age);
+
+% Enforce b is within the boundaries (without this step, interp2 in value_working might return NaN even if b - bv_nb = 1e-15)
+b = min(bv_nb, b);
 
 end
 
