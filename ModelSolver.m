7a3c85e44cb032d1e1c2dedad9d2819a0635e7dd
Daniela <danielav@wharton.upenn.edu>
2018-01-25 16:36:01 -0500

    Revert "Made survival probability z state-dependent, so now it accounts for both demographic type and productivity."
    This reverts commit a183ec35b95543c3ab8de87fffaee587b450455e.

--- ModelSolver.m
+++ ModelSolver.m
@@ -346,7 +346,7 @@ methods (Static)
             f = @(F) sum(sum(reshape(DIST_gs .* F, [], T_model), 1), 3);
             
             Aggregate.pops     = f(1);                                                                                   % Population
-            Aggregate.bequests = f(OPTs.K .* repmat(reshape(1-surv, [nz,1,1,T_life,1]), [1,nk,nb,1,T_model]));           % Bequests
+            Aggregate.bequests = f(OPTs.K .* repmat(reshape(1-surv, [1,1,1,T_life,1]), [nz,nk,nb,1,T_model]));           % Bequests
             Aggregate.labs     = f(OPTs.LAB);                                                                            % Labor
             Aggregate.labeffs  = f(OPTs.LAB .* repmat(reshape(zs, [nz,1,1,T_life,1]), [1,nk,nb,1,T_model]));             % Effective labor
             Aggregate.lfprs    = f(OPTs.LAB > 0.01) ./ f(1);                                                             % Labor force participation rate
