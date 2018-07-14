8f1b037c09602ead35e0b1c74eee802099e7d5d8
Nick Janetos <njanetos@wharton.upenn.edu>
2018-07-14 14:51:37 -0400

    Increase parameter set

--- CombinationSolver.m
+++ CombinationSolver.m
@@ -133,9 +133,9 @@ methods (Static)
         
         % Define default output parameter value sets
         output_parameters0.OpenEconomy          = {0, 0.4, 0.7, 1};
-        output_parameters0.UseDynamicBaseline   = {false};
+        output_parameters0.UseDynamicBaseline   = {false, true};
         output_parameters0.LaborElasticity      = {0.5};
-        output_parameters0.IsLowReturn          = {false};
+        output_parameters0.IsLowReturn          = {false, true};
         
         % Identify output parameter value sets, applying defaults where unspecified
         if exist('output_parameters', 'var') && ~isempty(output_parameters)
