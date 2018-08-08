9439b874620d43ddc90d5e1a5f04bea7ff19f1a1
Efraim <efraim@wharton.upenn.edu>
2018-08-08 12:42:13 -0400

    Removed unused old_calculateKLRatio function.

--- Firm.m
+++ Firm.m
@@ -234,98 +234,7 @@ classdef Firm < handle
         end % calculateKLRatio
 
         
-        %% Test
-        function  [] = testMe( this )
-            
-            T_model = 25;
-            
-            % Make sample inputs
-            effectiveDividendRate = ones(T_model,1) .* 0.05;
-            labor = ones(T_model,1);
-            init_caps = ones(T_model,1) * 12;
-            invtocap_Tmodel = 0.07;
-            
-            [klRatio tCaps]     = this.calculateKLRatio( effectiveDividendRate   , ...
-                                        init_caps, labor        , ...
-                                        invtocap_Tmodel );
-                        
-             tempCaps   = klRatio .* labor;
-             tempI      = [diff(tempCaps); invtocap_Tmodel * tempCaps(T_model)];
-             tempWages  = this.TFP*(1-this.capitalShare)*(klRatio.^this.capitalShare);
-             
-             [tempDividends, ~]  = this.dividends( tempCaps          ...
-                                                     ,   tempI     ...
-                                                     ,   klRatio            ...
-                                                     ,   tempWages           ...
-                                                     );  
-              tempDivRates = (tempDividends ./ (tempCaps .* this.priceCapital));                                   
-
-              fprintf('diff in dividends');
-              tempDivRates - effectiveDividendRate
-
-        end %testMe
         
-        
-        %% 
-        % Calculate K/L ratio from dividend rate
-        function [KLratio, tempCaps] = old_calculateKLRatio( this, dividendRate, labor, invtocapsT_model )
-            % Inputs : dividendRate = dollars received as dividend per
-            %                         dollar owned of equity
-            %          labor = efficient units of labor (from last iteration)
-            %          invtocapsT_model = last period guess of I/K
-            % Outputs: KLratio that generates the exact dividendRate of inputs
-            
-            % Initialize variables
-            T_model            = length(dividendRate);
-            caps               = zeros(T_model+1,1);
-            invtocaps          = zeros(T_model,1);
-            invtocaps(T_model) = invtocapsT_model;
-            
-            % Find K at T_model
-            % Total expensing divided by capital (expensing subsidy rate)
-            exptocaps      = this.expensingRate(T_model) * this.corpTaxRate(T_model) ...
-                             * invtocaps(T_model);
-            % MPK
-            MPK            = ( dividendRate(T_model) + this.depreciationRate ...
-                                + this.riskPremium - exptocaps     ...
-                              ) * this.priceCapital(T_model) / (1 - this.corpTaxRate(T_model))            ;
-            % KLratio
-            KLratioT_model = ( MPK / (this.TFP * this.capitalShare) ) ^ (1/(this.capitalShare-1));
-            % Capital at T_model
-            caps(T_model)  = KLratioT_model * labor(T_model);
-            
-            % Find capital sequence by backward induction numerically solving
-            % the polynomial: Psi*k(t) - Gamma*k(t)^alpha - Phi(k(t+1)) = 0 
-            for t = T_model-1:-1:1
-                
-                % Constants
-                Psi = ( dividendRate(t) + this.depreciationRate + this.riskPremium ...
-                       + this.expensingRate(t) * this.corpTaxRate(t) * (1 - this.depreciationRate) ... 
-                       ) * this.priceCapital(t) ...
-                      / ( this.TFP * this.capitalShare * (1 - this.corpTaxRate(t)) );
-                Phi = caps(t+1) * ...
-                      ( ( this.expensingRate(t) * this.corpTaxRate(t) * this.priceCapital(t) ) ...
-                      / (  this.TFP * this.capitalShare * (1 - this.corpTaxRate(t)) ));                
-                Gamma = labor(t)^(1 - this.capitalShare);
-                
-                % Find polynomial root
-                % caps(t+1) is used as the guess
-                [x, ~, exitflag] = fzero(@(x) (x - (Gamma/Psi)*(x^this.capitalShare) - Phi/Psi), caps(t+1) );
-                
-                if ( exitflag ~= 1)
-                    error('No root to the polynomial.')
-                end
-                
-                % Save time t capital
-                caps(t) = x;
-               
-            end
-            
-            % Calculate capital-labor ratio
-            KLratio = caps(1:T_model) ./ labor;
-            tempCaps = caps(1:T_model);
-        end % calculateKLRatio
-
         
         %% 
         % Calculate optimal debt 
@@ -373,7 +282,38 @@ classdef Firm < handle
         end % calculateLeverageCost
 
 
-        
+
+        %% Test
+        function  [] = testMe( this )
+            
+            T_model = 25;
+            
+            % Make sample inputs
+            effectiveDividendRate = ones(T_model,1) .* 0.05;
+            labor = ones(T_model,1);
+            init_caps = ones(T_model,1) * 12;
+            invtocap_Tmodel = 0.07;
+            
+            [klRatio tCaps]     = this.calculateKLRatio( effectiveDividendRate   , ...
+                                        init_caps, labor        , ...
+                                        invtocap_Tmodel );
+                        
+             tempCaps   = klRatio .* labor;
+             tempI      = [diff(tempCaps); invtocap_Tmodel * tempCaps(T_model)];
+             tempWages  = this.TFP*(1-this.capitalShare)*(klRatio.^this.capitalShare);
+             
+             [tempDividends, ~]  = this.dividends( tempCaps          ...
+                                                     ,   tempI     ...
+                                                     ,   klRatio            ...
+                                                     ,   tempWages           ...
+                                                     );  
+              tempDivRates = (tempDividends ./ (tempCaps .* this.priceCapital));                                   
+
+              fprintf('diff in dividends');
+              tempDivRates - effectiveDividendRate
+
+        end %testMe
+
     end % instance methods
     
 end % classdef
